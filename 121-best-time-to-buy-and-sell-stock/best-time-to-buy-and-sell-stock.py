c3f69678e76a0aa131a41fc13cc8b27ea1e8794d
Moein <moein9805@gmail.com>
2025-10-30 00:17:29 +0600

    Time: 99 ms (48.16%) | Memory: 26.9 MB (77.76%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -2,7 +2,8 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
     ## Dynamic programming solution
-        komDam = prices[0]
+    
+        """komDam = prices[0]
         motLabh = 0 
 
         for sell in prices:
@@ -12,6 +13,24 @@ class Solution:
             komDam = min(komDam , sell)
 
         return motLabh
+        """
+        # Two pointer solution 
+        bam , dan = 0 , 1
+
+        motLabh = 0
+
+        while dan < len(prices):
+            if prices[bam] < prices[dan]:
+
+                Labh = prices[dan] - prices[bam]
+                motLabh = max(motLabh , Labh)
+            else:
+                bam = dan
+            dan += 1
+
+        return motLabh
+
+
 
             
        
